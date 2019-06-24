f70f9c0400fbd350dfdc73c7cb463e6f6ac75864
Mary West <westmarye1219@gmail.com>
2019-06-24 18:52:33 -0500

    h1 tags appear on the dom

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className = "btn btn-danger">Fishstore</button>
-      </header>
+        <Auth />
+        <Home />
     </div>
   );
 }
