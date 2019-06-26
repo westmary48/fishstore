11b11bd7f79ff36fc45d90f686291f8c544af5bf
Mary West <westmarye1219@gmail.com>
2019-06-25 21:09:57 -0500

    added axios call, my data is showing up in react console

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import ordersData from '../../helpers/data/ordersData';
 
 class Orders extends React.Component {
+  state = {
+    orders: [],
+  }
+
+  componentDidMount() {
+    ordersData.getMyOrders(firebase.auth().currentUser.uid)
+      .then(orders => this.setState({ orders }))
+      .catch(err => console.error('cant get orders', err));
+  }
+
   render() {
     return (
       <h2>Orders</h2>
