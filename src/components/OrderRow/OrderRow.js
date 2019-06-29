b9ff5275abcee84761f1976d31645c1abc4df04d
Mary West <westmarye1219@gmail.com>
2019-06-29 12:39:44 -0500

    amount of orders showing up on screen

--- src/components/OrderRow/OrderRow.js
+++ src/components/OrderRow/OrderRow.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import ordersData from '../../helpers/data/ordersData';
+
+import moment from 'moment';
+
+import orderShape from '../../helpers/propz/orderShapes';
 
 class OrderRow extends React.Component {
+  static propTypes = {
+    orders: orderShape.orderShape,
+  };
+
   render() {
     const { order } = this.props;
+    const numFish = Object.values(order.fishes).reduce((a, b) => a + b);
     return (
       <tr>
-        <th>{order.id}</th>
-        <td>{order.dateTime}</td>
-        <td>5</td>
+        <th>{order.name}</th>
+        <td>{moment(order.dateTime).format('LLL')}</td>
+        <td>{numFish}</td>
         <td><button className = "btn btn-danger">X</button></td>
-
       </tr>
     );
   }
