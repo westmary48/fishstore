d7cb925646b4435dc5de726e6e8648d912cc695e
Mary West <westmarye1219@gmail.com>
2019-07-01 18:33:06 -0500

    save order button shows up after add to order button is clicked

--- src/components/NewOrder/NewOrder.js
+++ src/components/NewOrder/NewOrder.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
+import './NewOrder.scss';
+
 class NewOrder extends React.Component {
   render() {
+    const { fishOrder } = this.props;
+    const orderIds = Object.keys(fishOrder);
+    const orderExists = orderIds.length > 0;
     return (
-      <h2>NewOrder</h2>
+      <div className="NewOrder">
+        <h1>New Order</h1>
+        <form className='col-6 offset-3'>
+          <div className="form-group">
+            <label htmlFor="order-name">Order Name:</label>
+            <input
+              type="text"
+              className="form-control"
+              id="order-name"
+              placeholder="John's Order"
+            />
+          </div>
+        </form>
+        <ul>{}</ul>
+        <div className="total">
+          Total: <strong>SOME TOTAL HERE</strong>
+        </div>
+        <div className="text-center">
+          {
+            orderExists ? (
+              <button className="btn btn-outline-dark"> Save Order </button>
+            ) : (
+              <div>Add Inventory to your order</div>
+            )
+          }
+        </div>
+      </div>
     );
   }
 }
